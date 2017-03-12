ba75c2c6f78679e96d3423cd5381fe8874830a02
nabn <nabeen.khadka@gmail.com>
2017-03-12 10:42:22 +0545

    Setup git

---
+++ index.js
@@ -0,0 +1,19 @@
+const ex = require('express')
+const request = require('request')
+
+const app = ex()
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.use('/', function (req, res) {
+  try {
+    const url = 'https://bluesage-dev.bluesageusa.com'+ req.url
+    req.pipe(request(url)).pipe(res)
+  } catch (e) {console.log('error', e)}
+})
+
+app.listen(process.env.PORT || 3030)
