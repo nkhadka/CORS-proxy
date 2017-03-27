5e1c199cb15373e04a274812eae54257622da84c
nabn <nabeen.khadka@gmail.com>
2017-03-27 17:31:40 +0545

    Improve logging logic

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const request = require('request')
 const app = ex()
 // const BASE_URL = 'https://bcvguwpzrwpcxxq4w-mock.stoplight-proxy.io'
 // const BASE_URL = 'https://bluesage-api.bluesageusa.com'
-// const BASE_URL = 'https://homebridge-dev.bluesageusa.com'
-const BASE_URL = 'http://btech-dev2.bluesageusa.com:8081'
+const BASE_URL = 'https://homebridge-dev.bluesageusa.com'
+// const BASE_URL = 'http://btech-dev2.bluesageusa.com:8081'
 // const BASE_URL = 'https://bluesage-dev.bluesageusa.com'
+//
+console.log('CORSPROXY started')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
@@ -17,8 +19,8 @@ app.use(function(req, res, next) {
 app.use('/', function (req, res) {
   try {
     const url = BASE_URL + req.url
-    req.pipe(request(url)).pipe(res)
-    console.log(req.url)
+    console.log(req.url.split('access_token')[0])
+    req.url !== '/' && req.pipe(request(url)).pipe(res)
   } catch (e) {console.log('error', e)}
 })
 
